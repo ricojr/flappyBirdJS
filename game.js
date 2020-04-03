e15d1ae84f9dba9980e2346c300405e51075a721
ricojr <ricopehangko@gmail.com>
2020-04-03 16:31:38 +0200

    added animations sounds

--- game.js
+++ game.js
@@ -9,6 +9,22 @@ const DEGREE = Math.PI/180;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+// load sounds
+const SCORE_S = new Audio();
+SCORE_S.src = "audio/sfx_point.wav";
+
+const FLAP = new Audio();
+FLAP.src = "audio/sfx_flap.wav";
+
+const HIT = new Audio();
+HIT.src = "audio/sfx_hit.wav";
+
+const SWOOSHING = new Audio();
+SWOOSHING.src = "audio/sfx_swooshing.wav";
+
+const DIE = new Audio();
+DIE.src = "audio/sfx_die.wav";
+
 // game state
 const state = {
     current : 0,
@@ -30,9 +46,11 @@ canvas.addEventListener("click", function(event){
     switch(state.current){
         case state.getReady:
             state.current = state.game;
+            SWOOSHING.play();
             break;
         case state.game:
             bird.flap();
+            FLAP.play();
             break;
         case state.over:
             let rect = canvas.getBoundingClientRect();
@@ -154,6 +172,7 @@ const bird = {
                 this.y = canvas.height - foreground.h -this.h/2;
                 if(state.current == state.game){
                     state.current == state.over;
+                    DIE.play();
                 }
             }
             
@@ -265,18 +284,20 @@ const pipes = {
             // top pipe
             if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h){
                 state.current = state.over;
+                HIT.play();
             }
 
             // bottom pipe
             if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h){
                 state.current = state.over;
+                HIT.play();
             }
 
             // if pipes go beyond canvas, we delete them from the array
             if(p.x + this.w <= 0){
                 this.position.shift();
                 score.value += 1;
-
+                SCORE_S.play();
                 score.best = Math.max(score.value, score.best);
                 localStorage.setItem("best", score.best);
             }
